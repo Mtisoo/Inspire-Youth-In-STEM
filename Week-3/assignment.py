e485f078beb47e2a5e5e318ffce7bd3f138a5c19
Mtisoo <manujr254@gmail.com>
2023-02-20 17:44:08 +0300

    assignment

--- Week-3/assignment.py
+++ Week-3/assignment.py
@@ -2,3 +2,5 @@
 # add using for loop 5 new mucisians and copy the list to a new list called select
 # sort the list pop out two selects from the list
 # count the remaining selects from the list
+
+mucisians = []
