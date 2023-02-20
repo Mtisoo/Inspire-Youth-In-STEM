56cc4bb0164c728082d2c566e503ad763f6354d2
Mtisoo <manujr254@gmail.com>
2023-02-20 14:44:07 +0300

    lesson

--- Week-2/bank.py
+++ Week-2/bank.py
@@ -5,17 +5,16 @@
 #5% tax if income is less than 10k
 
 #print gross income and net income
-gross_income=input("Enter your gross income:")
+gross_income=int(input("Enter your gross income:"))
 if (gross_income <= 10000):
-    net_income=(-(5*gross_income)/100 +gross_income)
-    print("Your net income is: ", net income)
-elif (gross_income >=10000) & (gross_income <= 150000):
-    net_income = ((-(16*gross_income)/100) + gross income)
-    print("Since your gross income is {} your net income is{}."format. gross_income,net_income )
-elif (gross_income >= 150000) & (gross_income <= 300000):
-    net_income =( gross_income - ((19*gross_income)/100))
-    print("Since your gross income is {} your net income is{}."format. gross_income,net_income )
+    net_income=(gross_income-(5*gross_income)/100 )
+    print("Since your gross income is {} your net income is {}".format(gross_income,net_income) )
+elif (gross_income >=10000) & (gross_income < (150000)):
+    net_income = (gross_income -(16*gross_income)/100 )
+    print("Since your gross income is {} your net income is {}".format(gross_income,net_income) )
+elif (gross_income >= int(150000)) & (gross_income < 300000):
+    net_income =( gross_income - ((19/100)*gross_income))
+    print("Since your gross income is {} your net income is {}".format(gross_income,net_income) )
 else :
-    net_income = (gross_income - ((30*gross_income)/100))
-    print("Since your gross income is {} your net income is{}."format. gross_income,net_income )
-    
+    net_income = (gross_income - ((30/100)*gross_income))
+    print("Since your gross income is {} your net income is {}".format(gross_income,net_income) )
---
+++ Week-3/datatypes.py
@@ -0,0 +1,3 @@
+#camel case: First word starts with lowercase myFavouriteFruits
+#snake: all letters are in lowercase and words are separeted by underscores
+eg. date_of_week
